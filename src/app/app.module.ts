0b2d5628308d2fa380a306f4da7b01bd49c56f83
Drake Bennion <drakebennion@gmail.com>
2017-04-23 14:38:47 -0600

    adding angular router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact.component';
+import { HomeComponent } from './home.component';
+import { ProjectsComponent } from './projects.component';
+import { ResumeComponent } from './resume.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'resume', component: ResumeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/projects.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Project info coming soon</h1>`
+  //templateUrl: './home.component.html',
+  //styleUrls: ['./home.component.css']
+})
+export class ProjectsComponent {}
