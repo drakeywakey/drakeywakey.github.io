58b2d975ea7bd8ee34cd91657f1eebfc9508f934
Drake Bennion <drakebennion@gmail.com>
2017-04-23 20:15:13 -0600

    added findEm to the projects page

--- src/app/projects.component.ts
+++ src/app/projects.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Project info coming soon</h1>`
-  //templateUrl: './home.component.html',
-  //styleUrls: ['./home.component.css']
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {}
